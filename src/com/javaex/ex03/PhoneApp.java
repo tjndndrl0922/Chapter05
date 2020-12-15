7f669a80b2d56ecc63cdb26a4811eb9c7c99ea87
tjdndrl0922@naver.com <1@DESKTOP-BDRSOA3>
2020-12-15 10:48:45 +0900

    PhoneApp

--- src/com/javaex/ex03/PhoneApp.java
+++ src/com/javaex/ex03/PhoneApp.java
@@ -12,12 +12,23 @@ public class PhoneApp {
 		Reader fr = new FileReader("C:\\Users\\1\\Desktop\\새 폴더\\PhoneDB.txt");
 		BufferedReader br = new BufferedReader(fr);
 
+		String str = "";
+		
 		while (true) {
-			String str = br.readLine();
+			str = br.readLine();
 			if (str == null) {
 				break;
 			}
-			System.out.println(str);
+			
+			String[] person = str.split(",");
+			String name = person[0];
+			String hp = person[1];
+			String company = person[2];
+			
+			System.out.println("이름: "+name);
+			System.out.println("핸드폰: "+hp);
+			System.out.println("회사: "+company);
+			System.out.println("");
 		}
 		br.close();
 
--- src/com/javaex/ex04/PhoneApp.java
+++ src/com/javaex/ex04/PhoneApp.java
@@ -1,11 +1,15 @@
 package com.javaex.ex04;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class PhoneApp {
 	
@@ -13,12 +17,41 @@ public class PhoneApp {
 		
 		Reader fr = new FileReader("C:\\Users\\1\\Desktop\\새 폴더\\PhoneDB.txt");
 		BufferedReader br = new BufferedReader(fr);
-		br.close();
-		List<String> List = new ArrayList<String>();
+		
+		List<Person> list = new ArrayList<Person>();
 		String str;
-		while((str = br.readLine()) !=null) {
+		
+		Scanner sc = new Scanner(System.in);
+	
+		while(true) {
+			str = br.readLine();
+			if(str == null) {
+				break;
+			}
+			String[] pr = str.split(",");
+			
+			Person person = new Person(pr[0],pr[1],pr[2]);
+			list.add(person);
 			
 		}
+		
+		System.out.println(list.toString());
+		
+		Writer fw = new FileWriter("C:\\Users\\1\\Desktop\\새 폴더\\PhoneDB.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		for(Person p : list) {
+			String line = p.getName() + "," + p.getHp() + "," + p.getCompany();
+			bw.write(line);
+			bw.newLine();
+		}
+		
+		
+		
+		
+		
+		sc.close();
+		br.close();
 	}
 
 		
